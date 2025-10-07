25cf12845ab2e581e04cbd882c61aa772270c7c2
Nicolás Alegres <nicolasalgs@gmail.com>
2025-10-07 16:09:04 -0300

    fix: Corregir lógica de actualización del carrito y renderizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 import Product from './Product'
 
 function App() {
-  // 🧃 Creamos la mochila (estado)
-  const [cartCount, setCartCount] = useState(0)
+  // array de objetos
+
+  const [cart, setCart] = useState([])
 
   // 🔑 Función que suma 1 al carrito
-  const handleAddToCart = () => {
-    setCartCount(cartCount + 1)
+  const handleAddToCart = (productToAdd) => {
+    const existingProduct = cart.find(item => item.name === productToAdd.name);
+
+    if (existingProduct) {
+      setCart(cart.map(item =>
+        item.name === productToAdd.name ? { ...item, quantity: item.quantity + 1 }
+          : item
+      ));
+    } else {
+      setCart([...cart, { ...productToAdd, quantity: 1 }]);
+
+
+    }
   }
+
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
     <div>
       <h1>Mi Tienda</h1>
-      <p>🛒 Productos en el carrito: {cartCount}</p>
+      <p>🛒 Productos en el carrito: {totalItems}</p>
 
       {/* Le pasamos la función al componente Product */}
-      <Product name="Camiseta React" price={1500} onAdd={handleAddToCart} />
-      <Product name="Gorra JavaScript" price={800} onAdd={handleAddToCart} />
+      <Product name="Camiseta React"
+        price={1500}
+        onAdd={() => handleAddToCart({
+          name: "Camiseta React", price: 1500
+        })} />
+
+      <Product 
+      name="Gorra JavaScript" 
+      price={800}
+      onAdd={() =>handleAddToCart({ name: "Gorra JavaScript", price: 800 })} />
     </div>
   )
 }
